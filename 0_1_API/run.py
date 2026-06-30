416ab832866e3d59ad1d8b475d8489a25f14b741
Jhon <hantoni577@gmail.com>
2026-06-29 19:02:40 -0500

    Delete 0_1_API directory

--- 0_1_API/run.py
+++
@@ -1,5 +0,0 @@
-import uvicorn
-
-if __name__ == "__main__":
-    # Arranca el servidor buscando la instancia de "app" dentro de la carpeta app.main
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
